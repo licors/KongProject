ed8807e00cdf9f10170bfc435219922f166f1fbc
KH2\hyo <licors214@gmail.com>
2016-10-19 20:54:30 +0900

    로그인 화면 띄우는 부분 업로드 tiles 의 layout은 임의로 작성함(추후 수정해야함) 1. 프로젝트 설정>Reousrce>Text file encoding 부분에서 UTF-8 선택 2. Java Built Path 에서 JRE 버전 1.8로 수정 3. Web Project Settings 에서 프로젝트명을 / 로 변경

---
+++ src/main/java/kong2/member/controller/MemberDao.java
@@ -0,0 +1,26 @@
+package kong2.member.controller;
+
+import java.util.List;
+
+public interface MemberDao {
+
+	public MemberModel memberLogin(MemberModel mem);
+
+	public MemberModel getMember(String id);
+
+	public Object insertMember(MemberModel mem);
+
+	public MemberModel idFindByName(MemberModel member);
+	
+	public MemberModel pwFindById(MemberModel member);
+
+	public Object memberModify(MemberModel member);
+
+	public Object memberDelete(String id);
+	
+	public List<ZipcodeModel> zipcodeCheck(ZipcodeModel zipcodeModel) throws Exception;
+	
+}
+
+
+
